e5a4fc1bad33dab5551f59740ca11bf21816b423
pamirshinwari <pamir.forcode@gmail.com>
2024-12-30 12:56:02 -0500

    initial commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import datetime
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SEC_KEY", "khan")
+app.config['SECRET_KEY'] = os.environ.get("SEC_KEY")
 bootstrap = Bootstrap5(app)
 ckeditor = CKEditor(app)
 
@@ -198,4 +198,4 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
